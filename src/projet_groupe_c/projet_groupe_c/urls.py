6302f7d17d01df096dc55415dac159a0f4e2e22e
lhade <105942600+lhade@users.noreply.github.com>
2023-06-07 11:39:11 +0200

    create_itemv1

--- src/projet_groupe_c/projet_groupe_c/urls.py
+++ src/projet_groupe_c/projet_groupe_c/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from .views import create_item, item_list
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("create_item", create_item, name="create_item"),
 ]
---
+++ src/projet_groupe_c/projet_groupe_c/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+import requests
+from datetime import date
+
+
+# Create your views here.
+
+from django.http import HttpResponse, HttpRequest
+from .models import Item
+
+
+def create_item(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        url = request.POST.get("url")
+
+        item_object = Item.objects.create(
+            username=username, password=password, url=url, datetime=date.today()
+        )
+        item_object.save()
+
+        return render(request, "item_list.html")
+    return render(request, "create_item.html", context={})
+
+
+def item_list(request):
+    return render(request, "item_list.html")
